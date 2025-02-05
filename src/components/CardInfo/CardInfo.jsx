700767fa612694b532396b851fca3da1bdaff2cd
unknown <joan_mart_her@hotmail.com>
2025-02-05 13:16:07 +0100

    Mínimo hecho

---
+++ src/components/CardInfo/CardInfo.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./CardInfo.scss"
+import { Card, Button } from 'antd';
+
+const CardInfo = ({formData,setShow}) => {
+
+  return (
+    <Card title="Datos Enviados" style={{ marginTop: 20 }}>
+      <div className="info">
+        <p><strong>Name:</strong> {formData.name}</p>
+        <p><strong>Surname: </strong>{formData.surname}</p>
+        <p><strong>Email: </strong>{formData.email}</p>
+        <p><strong>Phone number: </strong>{formData.number}</p>
+        <p><strong>Message: </strong>{formData.message}</p>
+      </div>
+      <Button onClick={() => setShow(false)}>
+        Cerrar
+      </Button>
+    </Card>
+  )
+}
+
+export default CardInfo
---
+++ src/components/Formulario/Formulario.jsx
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import "./Formulario.scss";
+import { Button, Form, Input } from "antd";
+import CardInfo from "../CardInfo/CardInfo";
+
+const Formulario = () => {
+  const [dataForm, setDataForm] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    number: 0,
+    message: "",
+  });
+  const [show, setShow] = useState(false);
+  const validateMessages = {
+    required: "${label} is required!",
+    types: {
+      email: "${label} is not a valid email!",
+    },
+  };
+
+  const onFinish = (values) => {
+    setDataForm(values);
+    setShow(true);
+  };
+
+  return (
+    <div className="fondo">
+      <div className="fondo-form">
+        <Form
+          name="formulario"
+          onFinish={onFinish}
+          validateMessages={validateMessages}
+        >
+          <div className="titulo">Ejemplo formulario</div>
+          <div className="div-form">
+            <label>Name</label>
+            <Form.Item
+              name="name"
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter your name",
+                },
+              ]}
+            >
+              <Input className="input-form" placeholder="Name" />
+            </Form.Item>
+          </div>
+          <div className="div-form">
+            <label>Surname</label>
+            <Form.Item
+              name="surname"
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter your surname",
+                },
+              ]}
+            >
+              <Input className="input-form" placeholder="Surname" />
+            </Form.Item>
+          </div>
+          <div className="div-form">
+            <label>Email adress</label>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter your email adress",
+                },
+                {
+                  type: "email",
+                  message: "Please enter a valid email adress",
+                },
+              ]}
+            >
+              <Input className="input-form" placeholder="example@example.com" />
+            </Form.Item>
+          </div>
+          <div className="div-form">
+            <label>Phone number</label>
+            <Form.Item
+              name="number"
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter your phone number",
+                },
+                {
+                  pattern: /^\+?[1-9]\d{8,11}$/,
+                  message: "Please enter a valid phone number",
+                },
+              ]}
+            >
+              <Input className="input-form" placeholder="678 777 888" />
+            </Form.Item>
+          </div>
+          <div className="div-form">
+            <label>Message</label>
+            <Form.Item
+              name="message"
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter your message",
+                },
+              ]}
+            >
+              <Input
+                className="input-form"
+                placeholder="Write here your message"
+              />
+            </Form.Item>
+          </div>
+          {console.log(show)}
+          {show ? (
+            <div className="cardInfor">
+              <CardInfo formData={dataForm} setShow={setShow} />
+            </div>
+          ) : null}
+
+          <Form.Item label={null}>
+            <Button color="default" variant="solid" htmlType="submit">
+              Send
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default Formulario;
